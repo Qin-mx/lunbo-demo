3ec7474386fe4ca20e34329474408a0089e5b217
qin-mx <qmx_914@163.com>
2018-06-21 15:06:39 +0800

    轮播练习

---
+++ 插件实现轮播/index.js
@@ -0,0 +1,50 @@
+$(function(){
+    // 先获取需要操作的元素
+    var wrap = $('.wrap'),
+        banner = $('.banner'),
+        lis = $('.banner li'),
+        next = $('.next'),
+        prev = $('.prev'),
+        dots = $('.dots span');
+    var num = 0;  // 当前图片的下标
+    var timer = null; // 设置时间函数
+    
+    // 实现轮播图
+    function lunbo(){
+        banner.animate({marginLeft: -num*1200 +'px'},1000)
+        $.each(dots,function(index,item){
+            $(dots[index]).removeClass()
+        })
+      $( dots[num]).addClass('active')
+    }
+
+    function autoplay(){
+        timer = setInterval(function(){
+            num++;
+            num = num%lis.length;
+            lunbo()
+        },1000)
+    }
+    autoplay()
+    // 移入移除
+    wrap.hover(function(){clearInterval(timer)},function(){autoplay()})
+    // 点击
+    next.click(function(){
+        num++;
+        num = num%lis.length;
+        lunbo()
+    })
+    prev.click(function(){
+        num--;
+        num = num < 0? lis.length-1:num%lis.length;
+        lunbo()
+    })
+
+    $.each(dots,function(i){
+        $(dots[i]).click(function(){
+            num = this.id;
+            lunbo()
+        })
+    })
+
+})
---
+++ 通过left控制/index.js
@@ -0,0 +1,73 @@
+window.onload = function () {
+    // 获取轮播图需要的元素
+    var banner = document.getElementsByClassName('wrap')[0],
+        lis = banner.getElementsByTagName('li'),
+        prev = document.getElementsByClassName('prev')[0],
+        next = document.getElementsByClassName('next')[0],
+        dots = document.getElementsByClassName('dots')[0],
+        dotsli = dots.getElementsByTagName('span');
+
+    // 定义一个下标，表示当前要展示的图片
+    var num = 0;
+    var timer = null;
+
+    function lunbo(){
+        // 第一步取药遍历图片。更改left的值
+        for( var i = 0; i<lis.length; i++){
+            lis[i].style.left= i*1200 +'px';
+            dotsli[i].className =''
+        }
+        lis[num].style.left= 0;
+        dotsli[num].className = 'active'
+   }
+
+    
+    function autoPlay(){
+        timer = setInterval(function(){
+           num ++;
+           num = num%lis.length
+           lunbo()
+        },2000)
+    }
+
+    autoPlay()
+    
+    // 移入移除
+    banner.addEventListener('mouseover',function(){
+        clearInterval(timer)
+        
+    })
+    banner.addEventListener('mouseout',function(){
+        autoPlay()
+    })
+
+
+    // 点击左右切换
+
+    prev.addEventListener('click',function(){
+        num--;
+        clickNav()
+    })
+
+    next.addEventListener('click',function(){
+        num++;
+        clickNav()
+    })
+
+    function clickNav(){
+        num = num < 0? lis.length-1: num%lis.length;
+        lunbo()
+    }
+
+    //点击圆点切换
+    for( var i = 0; i<lis.length; i++){
+        dotsli[i].id =i // 设置当前的下标
+        dotsli[i].onclick = function(){
+            num = this.id;
+            lunbo()
+        }
+    }
+}
+
+
+
